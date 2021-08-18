1c998b416c2496a8ab117c1cae3d54c08cb58fc5
zx300487 <zx300487@ou.ac.uk>
2021-08-18 19:45:32 +0100

    More work had to deactivate the venv butwill workaround.  Netcat.py started.

---
+++ bhp/netcat.py
@@ -0,0 +1,21 @@
+import argparse
+import socket
+import shlex
+import subprocess
+import sys
+import textwrap
+import threading
+
+
+def execute(cmd):
+    cmd = cmd.strip()
+    if not cmd:
+        return
+    output = subprocess.check_output(
+        shlex.split(cmd), strerr=subprocess.STDOUT)
+
+    return output.decode()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='BHP NET TOOL')
